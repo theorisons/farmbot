6df5ea94d2cb732a9249e93e011eb21420b66161
Théorisons <34948793+theorisons@users.noreply.github.com>
2019-06-02 11:01:00 +0200

    Update FluxVideo.js

--- farmbot_webapp/src/components/FluxVideo.js
+++ farmbot_webapp/src/components/FluxVideo.js
@@ -1,39 +1,18 @@
 import React from "react";
 
-import { Spinner, Row, Col } from "react-bootstrap";
+import Iframe from "react-iframe";
 import { fluxVideoUrl } from "../fonctions/communicationsServeur";
 
 export default class FluxVideo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chargement: true
-    };
-  }
-
-  componentDidMount() {
-    //On veut recevoir de la video
-    console.log("OK");
-    //this.setState({chargement : false});
-  }
-  /*
-        envoiServeur() {
-    
-            fetch(fluxVideoUrl())
-               
-                .then(resultat => resultat.json())
-                .then(resultat => this.remiseZero(resultat.message, "#0F0"))
-                .catch(error => this.remiseZero(error.message, "#F00"));
-        }
-    */
-
-  chargement() {
-    if (this.state.chargement) {
-      return <Spinner animation="grow" />;
-    }
-  }
-
   render() {
-    return <div>{this.chargement()}</div>;
+    return (
+      <div>
+        <Iframe
+          url={fluxVideoUrl}
+          width="480px"
+          height="270px"
+        />
+      </div>
+    );
   }
 }
